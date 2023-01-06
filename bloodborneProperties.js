03057b710bd6e96a42de0eaea412072c5e2c89cf
goldenfriend <gquinlan@uoguelph.ca>
2023-01-06 17:35:15 -0500

    Changed bloodborneProperties.js, module.json

--- bloodborneProperties.js
+++ bloodborneProperties.js
@@ -1,4 +1,9 @@
 Hooks.on('ready', () => {
-CONFIG.DND5E.weaponProperties['arcane'] = 'Arcane';
-CONFIG.DND5E.weaponProperties['trick'] = 'Trick';
+    CONFIG.DND5E.weaponProperties['arcane'] = 'Arcane';
+    CONFIG.DND5E.weaponProperties['long-reach'] = 'Long-Reach';
+    CONFIG.DND5E.weaponProperties['piercing'] = 'Piercing';
+    CONFIG.DND5E.weaponProperties['scatter'] = 'Scatter';
+    CONFIG.DND5E.weaponProperties['serrated'] = 'Serrated';
+    CONFIG.DND5E.weaponProperties['trick'] = 'Trick';
+    CONFIG.DND5E.weaponProperties['unwieldly'] = 'Unwieldly';
 });
